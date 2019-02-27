945dacd87d15d208ed1faa4d66f7f3dbed627c4a
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-02-27 15:15:32 +1100

    Initial Commit
    Yeeted

---
+++ Artifical Intelligience/AI/Assets/GoHome/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform WaypointParent;
+    public float moveSpeed = 15f;
+    public float stoppingDistance = 1f;
+
+    public Transform[] waypoints;
+    private int currentIndex = 1;
+    private NavMeshAgent agent;
+    void Start()
+    {
+        waypoints = WaypointParent.GetComponentsInChildren<Transform>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        Patrol();
+    }
+    void Patrol()
+    {
+        Transform point = waypoints[currentIndex];
+        float distance = Vector3.Distance(transform.position, point.position);
+        if (distance < stoppingDistance)
+        {
+            currentIndex++;
+        }
+        if (currentIndex == waypoints.Length)
+        {
+            currentIndex = 1;
+        }
+        //transform.position = Vector3.MoveTowards(transform.position, point.position, moveSpeed*Time.deltaTime);
+        agent.SetDestination(point.position);
+    }
+}
