a433e2b82c4501a970eb3ef14da880f2d91e5236
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-03-01 23:17:27 +1100

    Updated

---
+++ Arcade Game/Unnamed Project/Assets/Scripts/Boss.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+    //Integers:
+    private int aTimer = 0;
+    private int jTimer = 0;
+    private int sTimer = 0;
+
+    //Booleans:
+    private bool startPeriod = true;
+    private bool canAttack = false;
+    private bool jumpAttack = false;
+    private bool chargeAttack = false;
+    private bool burstAttack = false;
+
+    //Array:
+    int[][] Setup;
+
+    void Start ()
+    {
+		
+	}
+
+    void FixedUpdate()
+    {
+
+    }
+
+    public void CalcAttack()
+    { }
+
+    public void Charge()
+    { }
+
+    public void Jump()
+    { }
+
+    public void Burst()
+    { }
+}
