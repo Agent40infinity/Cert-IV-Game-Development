81da38c7b630f1f5298c8727b3b2437e3eaf2971
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-03-05 15:44:16 +1100

    oml

---
+++ Programming/A Name/Assets/Scripts/SliderHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SliderHealth
+{
+	[AddComponentMenu("Linear Health")]
+	public class SliderHealth : MonoBehaviour 
+	{
+		[Header("Player Health")]
+		public int maxHealth = 100;
+		public int curHealth;
+
+		[Header("UI Reference")]
+		public Slider healthBar;
+		public Image sliderFill;
+
+		public void Start()
+		{
+			curHealth = maxHealth;
+		}
+		public void Update () 
+		{
+			healthBar.value = Mathf.Clamp01(curHealth / maxHealth);
+		}	
+	}
+}
