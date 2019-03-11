1f6ac4c78c1cbcc9a734c824b6224b0b00cde3a7
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-03-11 13:37:46 +1100

    All Arrays fixed

--- Arcade Game/Dorchalys - Arcade Game/Assets/Scripts/Tileset.cs
+++ Arcade Game/Dorchalys - Arcade Game/Assets/Scripts/Tileset.cs
@@ -10,7 +10,7 @@ namespace Tileset
         public GameObject tile_32;
         public int tileWidth = 20; //Sets the max value for tile Width;
         public int tileHeight = 15; //Sets the max value for tile Height;
-        public int[,] tileset_Layer1 =  //Top tileset layer for world initialization.
+        public float[,] tileset_Layer1 =  //Top tileset layer for world initialization.
             {
             {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
             {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
@@ -79,11 +79,11 @@ namespace Tileset
                     //Ignore Me ------->       print("tileWidth = " +tileWidth+ " Position("+x+ "," + y +") = " + tileset_Layer1[y, x]);
                     if (tileset_Layer1[y,x] == 0)
                     {
-                        Instantiate(tile_32, new Vector3 (x*32 + 32, y*32 + 32, 0), transform.rotation);
-                    }
+                        Instantiate(tile_32, new Vector3(x, y, 0), transform.rotation);
+                    } 
                     if (tileset_Layer1[y,x] == 1)
                     {
-                        Instantiate(tile_32, new Vector3 (x*32 + 32, y * 32 + 32, 0), transform.rotation);
+                        Instantiate(tile_32, new Vector3(x, y, 0), transform.rotation);
                     }
                     if (tileset_Layer1[y,x] == 2)
                     {
@@ -96,6 +96,60 @@ namespace Tileset
                     if (tileset_Layer1[y,y] == 4)
                     {
                         
+                    }
+                }
+            }
+            for (int y = 0; y < tileHeight; y++) //Checks the  Y position and value within the array to set it's content.
+            {
+                for (int x = 0; x < tileWidth; x++) //Checks the  X position and value within the array to set it's content.
+                {
+                    //Ignore Me ------->       print("tileWidth = " +tileWidth+ " Position("+x+ "," + y +") = " + tileset_Layer1[y, x]);
+                    if (tileset_Layer2[y, x] == 0)
+                    {
+                        Instantiate(tile_32, new Vector3(x, y, -1), transform.rotation);
+                    }
+                    if (tileset_Layer2[y, x] == 1)
+                    {
+                        Instantiate(tile_32, new Vector3(x, y, -1), transform.rotation);
+                    }
+                    if (tileset_Layer2[y, x] == 2)
+                    {
+
+                    }
+                    if (tileset_Layer2[y, x] == 3)
+                    {
+
+                    }
+                    if (tileset_Layer2[y, y] == 4)
+                    {
+
+                    }
+                }
+            }
+            for (int y = 0; y < tileHeight; y++) //Checks the  Y position and value within the array to set it's content.
+            {
+                for (int x = 0; x < tileWidth; x++) //Checks the  X position and value within the array to set it's content.
+                {
+                    //Ignore Me ------->       print("tileWidth = " +tileWidth+ " Position("+x+ "," + y +") = " + tileset_Layer1[y, x]);
+                    if (tileset_Layer3[y, x] == 0)
+                    {
+                        Instantiate(tile_32, new Vector3(x, y, -2), transform.rotation);
+                    }
+                    if (tileset_Layer3[y, x] == 1)
+                    {
+                        Instantiate(tile_32, new Vector3(x, y, -2), transform.rotation);
+                    }
+                    if (tileset_Layer3[y, x] == 2)
+                    {
+
+                    }
+                    if (tileset_Layer3[y, x] == 3)
+                    {
+
+                    }
+                    if (tileset_Layer3[y, y] == 4)
+                    {
+
                     }
                 }
             }
