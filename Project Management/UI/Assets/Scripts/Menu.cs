46437ed07cb171fc929564db3d8b404f36a99ec2
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-03-13 10:50:43 +1100

    UI for Byaarl Defense

---
+++ Project Management/UI/Assets/Scripts/Menu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject options;
+    public GameObject keybinds;
+	public void LoadScene(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+	}
+    public void Exit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+    public void ToggleSettings(int toggle)
+    {
+        if (toggle == 0)
+        {
+            menu.SetActive(false);
+            options.SetActive(true);
+        }
+        else if (toggle == 1 || Input.GetButton("Cancel"))
+        {
+            menu.SetActive(true);
+            options.SetActive(false);
+        }
+        else if (toggle == 2 || Input.GetButton("Cancel"))
+        {
+
+        }
+    }
+}
