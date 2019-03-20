c7a6ca435cab616dbf6966b5b9a46ea4755a0add
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-03-20 15:55:40 +1100

    x

---
+++ Artifical Intelligience/Tower Defense/Assets/Scripts/Selection/Selector.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selector : MonoBehaviour
+{
+    public GameObject[] towers;
+    public GameObject[] holograms;
+
+    private int currentTower = 0;
+
+    private Vector3 placeablePoint;
+
+	void OnDrawGizmos ()
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Gizmos.DrawLine(mouseRay.origin, mouseRay.origin + mouseRay.direction * 1000f);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(placeablePoint, 0.5f);
+	}
+	void Update ()
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition); //Initualize raycast
+        RaycastHit hit;
+        if (Physics.Raycast(mouseRay, out hit)) //Perform raycasting
+        {
+            Placeable p = hit.collider.GetComponent<Placeable>(); //Is placeable
+            if (p) //Is it placeable
+            {
+                placeablePoint = p.transform.position;
+            }
+        }
+	}
+
+    public void SelectTower(int tower)
+    {
+        if (tower >= 0 && tower < towers.Length) //Checks if the tower is within range of the array
+        {
+            currentTower = tower; //Set current tower to tower index
+        }
+    }
+}
