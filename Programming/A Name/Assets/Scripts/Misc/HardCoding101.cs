0ad4770b9fb4fa244abae73764d42bde8eb65220
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-03-25 15:55:12 +1100

    Hard coding finally

---
+++ Programming/A Name/Assets/Scripts/Misc/HardCoding101.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HardCoding101 : MonoBehaviour
+{
+    public Vector2 scrt;
+    private void OnGUI()
+    {
+        if (scrt.x != Screen.width / 16)
+        {
+            scrt.x = Screen.width / 16;
+            scrt.y = Screen.height / 9;
+        }
+
+        for (int x = 0; x < 17; x++)
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                GUI.Box(new Rect(scrt.x * x, scrt.y * y, scrt.x, scrt.y), "");
+                //GUI.Box(new Rect((scrt.x * x) - scrt * , 0, scrt.x, scrt.y), "");
+            }
+        }
+    }
+}
---
+++ Programming/A Name/Assets/Scripts/UI/EnemyHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float maxHP = 100;   
+    public float curHP;
+    public Canvas enemyHealth;
+    public Slider healthSlider;
+
+    public void Start()
+    {
+        enemyHealth = this.transform.Find("EnemyHealth").GetComponent<Canvas>();
+        healthSlider = enemyHealth.transform.Find("Slider").GetComponent<Slider>();
+        curHP = maxHP;
+    }
+    public void Update ()
+    {
+        healthSlider.value = Mathf.Clamp01(curHP / maxHP);
+        enemyHealth.transform.LookAt(Camera.main.transform);
+	}
+}
