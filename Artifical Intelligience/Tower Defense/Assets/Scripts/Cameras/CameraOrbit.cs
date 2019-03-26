304622d06471a146fe447fa7b1bb63fdc7f33a56
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-03-26 15:58:04 +1100

    camera stuff

---
+++ Artifical Intelligience/Tower Defense/Assets/Scripts/Cameras/CameraOrbit.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour
+{
+    public Camera attachedCamera;
+    [Header("Orbit")]
+    public float xSpeed = 120f, ySpeed = 210f;
+    public float yMinLimit = -20f, yMaxLimit = 80f;
+    [Header("Collision")]
+    public bool cameraCollision = true; //Is camera collision enabled?
+    public bool ignoreTrggers = true; //Will the spherecast ignore triggers
+    public float castRadius = .3f; //Radius of sphere to cast
+    public float castDistance = 1000f; //Distance the cast travels
+    public LayerMask hitLayers; //Layers that casting will hit
+
+    private float originalDistance; //Record starting distance of camera
+    private float distance; //Current distance of camera
+    private float x, y; //X and Y mouse rotation
+
+	void Start ()
+    {
+        originalDistance = Vector3.Distance(transform.position, attachedCamera.transform.position); //Set original distanceS
+        x = transform.eulerAngles.y; //Set X and Y degress to current camera rotation
+        y = transform.eulerAngles.x;
+
+    }
+	
+	void Update ()
+    {
+        if (Input.GetMouseButton(1)) //Is right mouse button pressed
+        {
+            //Disable Cursor:
+            Cursor.visible = false; 
+            Cursor.lockState = CursorLockMode.Locked;
+
+            //Orbit:
+            x += Input.GetAxis("Mouse X") * xSpeed * Time.deltaTime;
+            y -= Input.GetAxis("Mouse Y") * ySpeed * Time.deltaTime;
+            y = Mathf.Clamp(y, yMinLimit, yMaxLimit);
+            transform.rotation = Quaternion.Euler(y, x, 0); //Rotate the transform using Euler angles
+        }
+        else
+        {
+            Cursor.visible = true; //Enable Cursor
+            Cursor.lockState = CursorLockMode.None;
+        }
+	}
+
+
+    private void FixedUpdate()
+    {
+        distance = originalDistance; //Set distance to original distance
+        if (cameraCollision) //Change distance to what we hit
+        {
+            Ray camRay = new Ray(transform.position, -transform.forward);
+            RaycastHit hit; //Stores the hit inforation after cast
+            if (Physics.SphereCast(camRay, castRadius, out hit, castDistance, hitLayers, ignoreTrggers ? QueryTriggerInteraction.Ignore : QueryTriggerInteraction.Collide))
+            {
+                distance = hit.distance;
+            }
+        }
+        attachedCamera.transform.position = transform.position - transform.forward * distance; //Apply distance to cameras
+    }
+}
---
+++ Artifical Intelligience/Tower Defense/Assets/Scripts/Cameras/CameraPan.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour
+{
+    public Camera attachedCamera;
+    public float movementThreshold = .25f;
+    public float movementSpeed = 20f;
+    public float zoomSensitivity = 10;
+    public Vector3 size = new Vector3(20f, 1f, 20f);
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, size);
+    }
+
+    Vector3 GetAdjustedPos(Vector3 incomingPos)
+    {
+        Vector3 pos = transform.position;
+        Vector3 halfSize = size * .5f;
+
+        //X
+        if (incomingPos.x > pos.x + halfSize.x) incomingPos.x = pos.x + halfSize.x;
+        if (incomingPos.x < pos.x - halfSize.x) incomingPos.x = pos.x - halfSize.x;
+
+        //Y
+        if (incomingPos.y > pos.y + halfSize.y) incomingPos.y = pos.y + halfSize.y;
+        if (incomingPos.y < pos.y - halfSize.y) incomingPos.y = pos.y - halfSize.y;
+
+        //Z
+        if (incomingPos.z > pos.z + halfSize.z) incomingPos.z = pos.z + halfSize.z;
+        if (incomingPos.z < pos.z - halfSize.z) incomingPos.z = pos.z - halfSize.z;
+
+        return incomingPos;
+    }
+	void Update ()
+    {
+        Movement();
+	}
+    void Movement()
+    {
+        Transform camTransform = attachedCamera.transform; //Create a transform with a smaller name
+
+        Vector2 mousePoint = attachedCamera.ScreenToViewportPoint(Input.mousePosition); //Get mouse to viewport coordinates
+        Vector2 offset = mousePoint - new Vector2(.5f, .5f);
+
+        Vector3 input = Vector3.zero;
+        if (offset.magnitude > movementThreshold)
+            input = new Vector3(offset.x, 0, offset.y) * movementSpeed;
+
+        float inputScroll = Input.GetAxis("Mouse ScrollWheel"); //Get scroll from axis and multiply by zoomSensitivity
+        Vector3 scroll = camTransform.forward * inputScroll * zoomSensitivity;
+
+        Vector3 movement = input + scroll; //Apply movement
+        camTransform.position += movement * Time.deltaTime;
+
+        camTransform.position = GetAdjustedPos(camTransform.position);
+    }
+}
