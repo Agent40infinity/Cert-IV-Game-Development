d5270871c04c26d7f4b196007844b1f4e66ff948
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-04-08 12:58:17 +1000

    Changes

---
+++ Byaa'rrl Defense/Assets/Scripts/Utility/EndState.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndState : MonoBehaviour
+{
+    public GameObject overlay, end;
+    public GameObject win, lose;
+    public bool endState = false;
+    public bool won = false;
+    public bool loss = false;
+
+    void Update()
+    {
+        if (UI.health <= 0)
+        {
+            endState = true;
+            loss = true;
+        }
+        else if (Rounds.roundNum == 21)
+        {
+            endState = true;
+            won = true;
+        }
+        ToggleEndState();
+    }
+
+    public void ToggleEndState()
+    {
+        if (endState == true)
+        {
+            overlay.SetActive(false);
+            end.SetActive(true);
+            if (won == true)
+            {
+                win.SetActive(true);
+                lose.SetActive(false);
+            }
+            else if (loss == true)
+            {
+                lose.SetActive(true);
+                win.SetActive(false);
+            }
+        }
+        else if (endState == false)
+        {
+             overlay.SetActive(true);
+             end.SetActive(false);
+        }
+    }
+}
