fdf4376aab17d3671b90765e8c5fbeaad844f4b8
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-05-28 15:58:53 +1000

    x

--- Artifical Intelligience/Sunny Land/Assets/Scripts/Player.cs
+++ Artifical Intelligience/Sunny Land/Assets/Scripts/Player.cs
@@ -12,23 +12,55 @@ public class Player : MonoBehaviour
     public CharacterController2D controller;
     public float gravity = -10f;
     private Vector3 motion;
+    public float jumpHeight = 5f;
+    public Animator anim;
+    public SpriteRenderer rend;
 
-    void Reset()
+    void Start()
     {
         controller = GetComponent<CharacterController2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        float InputH = Input.GetAxis("Horizontal");
-        float InputV = Input.GetAxis("Vertical");
-        motion.x = InputV * jumpSpeed;
-        motion.x = InputH * movementSpeed;
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+        motion.y *= gravity * Time.deltaTime;
         if (controller.isGrounded)
         {
             motion.y = 0;
-        } 
-        motion.y += gravity * Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+        Climb(inputV);
+        Move(inputH);
         controller.move(motion * Time.deltaTime);
+    }
+
+    void Move(float inputH)
+    {
+        motion.x = inputH * movementSpeed;
+        anim.SetBool("isRunning", inputH != 0);
+        rend.flipx = inputH < 0;
+    }
+
+    void Climb(float inputV)
+    {
+
+    }
+
+    void Hurt()
+    {
+
+    }
+
+    void Jump()
+    {
+        motion.y = jumpHeight;
     }
 }
