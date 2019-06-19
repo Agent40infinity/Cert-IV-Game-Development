87c75acd734569f2bdffdf6b5e516e28f75f905d
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-06-19 22:31:46 +1000

    x

--- Game Systems/Minesweeper/Assets/Minesweeper/Scripts/Grid.cs
+++ Game Systems/Minesweeper/Assets/Minesweeper/Scripts/Grid.cs
@@ -11,14 +11,64 @@ namespace Minesweeper
         public float spacing = .155f;
 
         private Tile[,] tiles;
-        Tile SpawnTile(Vector3 pos)
+        Tile SpawnTile(Vector3 pos) //Creates a variable that allows for the creation of a new tile.
         {
             GameObject clone = Instantiate(tilePrefab);
             clone.transform.position = pos;
             Tile currentTile = clone.GetComponent<Tile>();
             return currentTile;
         }
-        public int GetAdjacentMineCount(Tile tile)
+
+        void Start() //Calls upon the specified function/sub-routine
+        {
+            GenerateTiles();
+        }
+
+        void GenerateTiles() //Used to generate the tiles within the array
+        {
+            tiles = new Tile[width, height];
+            for (int x = 0; x < width; x++) 
+            {
+                for (int y = 0; y < height; y++) //Sets up the spawning position for each tile based on the dimensions specified within the 2D array.
+                {
+                    Vector2 halfSize = new Vector2(width * 0.5f, height * 0.5f);
+                    Vector2 pos = new Vector2(x - halfSize.x, y - halfSize.y);
+                    Vector2 offset = new Vector2(.5f, .5f);
+                    pos += offset;
+                    pos *= spacing;
+                    Tile tile = SpawnTile(pos);
+                    tile.transform.SetParent(transform);
+                    tile.x = x;
+                    tile.y = y;
+                    tiles[x, y] = tile;
+                }
+            }
+        }
+
+        private void Update() //Checks whether or not we've clicked on a tile and begins the function/sub-routine.
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                SelectATile();
+            }
+        }
+
+        void SelectATile() //Used to check whether or not we've clicked on a tile; Creates a raycast as a check.
+        {
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mouseRay.origin, mouseRay.direction);
+            if (hit.collider != null) //If the raycast has hit something, then get the component required.
+            {
+                Tile hitTile = hit.collider.GetComponent<Tile>();
+                if (hitTile != null) //If the raycast hit 'Tile' then GetAdjacentMineCount and reveal tile.
+                {
+                    int adjacentMines = GetAdjacentMineCount(hitTile);
+                    hitTile.Reveal(adjacentMines);
+                }
+            }
+        }
+
+        public int GetAdjacentMineCount(Tile tile) //Get's the adjacent mine count to check whether or not a mine is near by and sets the sprite based on that value.
         {
             int count = 0;
             for (int x = -1; x <= 1; x++)
@@ -40,48 +90,41 @@ namespace Minesweeper
             }
             return count;
         }
-        void Start()
+
+        void FFuncover(int x, int y, bool[,] visited) //uncovers near by tiles that are black or equal to 1.
         {
-            GenerateTiles();
-        }
-        private void Update()
-        {
-            if (Input.GetMouseButtonDown(0))
+            if (x >= 0 && y >= 0 && x < width && y < height)
             {
-                SelectATile();
-            }
-        }
-        void GenerateTiles()
-        {
-            tiles = new Tile[width, height];
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
+                if (visited[x, y]) 
                 {
-                    Vector2 halfSize = new Vector2(width * 0.5f, height * 0.5f);
-                    Vector2 pos = new Vector2(x - halfSize.x, y - halfSize.y);
-                    Vector2 offset = new Vector2(.5f, .5f);
-                    pos += offset;
-                    pos *= spacing;
-                    Tile tile = SpawnTile(pos);
-                    tile.transform.SetParent(transform);
-                    tile.x = x;
-                    tile.y = y;
-                    tiles[x, y] = tile;
+                    return;
+                }
+                Tile tile = tiles[x, y];
+                int adjacentMines = GetAdjacentMineCount(tile);
+                tile.Reveal(adjacentMines);
+
+                if (adjacentMines == 0) //Used to check all tiles around the clicked tile.
+                {
+                    visited[x, y] = true;
+                    FFuncover(x - 1, y, visited);
+                    FFuncover(x + 1, y, visited);
+                    FFuncover(x, y - 1, visited);
+                    FFuncover(x, y + 1, visited);   
                 }
             }
         }
-        void SelectATile()
+
+        void UncoverMines() //Uncovers all mines if a single mine is clicked.
         {
-            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(mouseRay.origin, mouseRay.direction);
-            if (hit.collider != null)
+            for (int x = 0; x < width; x++) //Checks for the location of the mines.
             {
-                Tile hitTile = hit.collider.GetComponent<Tile>();
-                if (hitTile != null)
+                for (int y = 0; y < height; y++)
                 {
-                    int adjacentMines = GetAdjacentMineCount(hitTile);
-                    hitTile.Reveal(adjacentMines);
+                    Tile tile = tiles[x, y];
+                    if (tile.isMine) //Uncovers them.
+                    {
+                        int adjacentMines = GetAdjacentMineCount(tile);
+                    }
                 }
             }
         }
--- Game Systems/Minesweeper/Assets/Minesweeper/Scripts/Tile.cs
+++ Game Systems/Minesweeper/Assets/Minesweeper/Scripts/Tile.cs
@@ -7,22 +7,27 @@ namespace Minesweeper
     [RequireComponent(typeof(SpriteRenderer))]
     public class Tile : MonoBehaviour
     {
+        [Header("Personal")]
         public int x, y;
         public bool isMine = false;
         public bool isRevealed = false;
+
         [Header("References")]
         public Sprite[] emptySprites;
         public Sprite[] mineSprites;
         private SpriteRenderer rend;
-        void Awake()
+
+        void Awake() //Used to get the components required.
         {
             rend = GetComponent < SpriteRenderer>();
         }
-        void Start()
+
+        void Start() //Determines the chance of whether or not a tile spawns as a mine.
         {
-            isMine = Random.value < .05f;
+            isMine = Random.value < .15f;
         }
-        public void Reveal(int adjacentMines, int mineState = 0)
+
+        public void Reveal(int adjacentMines, int mineState = 0) //Allows a tile to be revealed and changes the sprite in accordance
         {
             isRevealed = true;
             if (isMine)
