aafe0efcb34448ceee4168b5b1c58b62287855ca
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-06-19 22:58:02 +1000

    Balls done

---
+++ Game Systems/100BallsGame/Assets/HundredBalls/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool open = false;
+    public Animator anim;
+
+    void Update()
+    { 
+        if (Input.GetMouseButtonDown(0)) //Checks if the player has pressed left click.
+        {
+            if (open) //checks if the hing is already open and closes it if it is. Does the opposite if not.
+            {
+                anim.SetTrigger("Close");
+                open = false;
+            }
+            else
+            {
+                anim.SetTrigger("Open");
+                open = true;
+            }
+        }
+    }
+}
